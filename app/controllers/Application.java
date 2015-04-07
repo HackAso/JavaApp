d348acbc9aa4f05e874c6a828f145bf68d9c91e8
Aki <sample@gmail.com>
2015-04-07 21:51:10 +0900

    a

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,28 +10,13 @@ import java.sql.*;
 public class Application extends Controller {
 
     public static Result index() {
+//        return ok(index.render("Yasunaga みてるー！？."));
+        return redirect()
 
-//        //test2
-//        Connection conn = null;
-//        String url = "jdbc:mysql://localhost:3306/HackAso";
-//        String user = "Akihiro";
-//        String password = "Akihiro";
-//        String message = null;
-//
-//        try{
-//            conn = DriverManager.getConnection(url, user, password);
-//            System.out.print("SQL接続出来ているみたい");
-//            Statement stmt = conn.createStatement();
-//            String sql = "SELECT * FROM sample";
-//            ResultSet rs = stmt.executeQuery(sql);
-//            message = rs.getString(1);
-//
-//            // データベースに対する処理
-//
-//        }catch (SQLException e) {
-//            message = "失敗";
-//        }
-        return ok(index.render("Yasunaga みてるー！？."));
     }
 
+
+    public static Result toSecond() {
+        return Results.TODO;
+    }
 }
